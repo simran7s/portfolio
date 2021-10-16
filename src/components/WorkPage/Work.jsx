147c89220fd8e4afdfe49b2a022461c979b3d31c
Simran Sandhu <simran7s@my.yorku.ca>
2021-10-16 00:38:23 -0400

    created some elements in work section

--- src/components/WorkPage/Work.jsx
+++ src/components/WorkPage/Work.jsx
@@ -4,8 +4,21 @@ function Work() {
     return (
         <>
             <section className="work" id="work">
-                <div className="spacer black-waves"></div>
-                <h1>hi</h1>
+                <div className="spacer black-waves">
+                </div>
+                <div className="container">
+                    <h3>My Work</h3>
+                    <ul className="work-nav">
+                        <li className="work-nav-item">All</li>
+                        <li className="work-nav-item">HTML/CSS/JS</li>
+                        <li className="work-nav-item">Node.js</li>
+                        <li className="work-nav-item">React</li>
+                    </ul>
+
+                    <div className="work-card">
+                        <h4>Pokemon</h4>
+                    </div>
+                </div>
             </section>
         </>
     )
